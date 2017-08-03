8a1573b1f4ab24713a0d0152fb1c25c39cd5b8b1
Juan Carlos Prieto from Dev <jprieto@nodriza.io>
2017-08-03 17:28:15 -0500

    test added

--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
 'use strict'
-import { Nodriza } from 'nodriza'
+import { Nodriza } from '../index'
 
 const params = {
   hostname: 'bp.nodriza.io',
@@ -7,3 +7,7 @@ const params = {
 }
 const nodriza = new Nodriza(params)
 
+nodriza.api.user.login({username: 'jprieto@nodriza.io', password: 'xxxxxx'}, (err, user) => {
+	console.log('->>> err:', err)
+	console.log('->>> user:', user)
+})
