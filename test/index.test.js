46cc300ac66753cde936dbc4efcc62cb64527008
nodriza-io <root@lapshox-2.local>
2017-08-06 11:16:19 -0500

    class extended

--- test/index.test.js
+++ test/index.test.js
@@ -1,25 +1,37 @@
 'use strict'
-import { Nodriza } from '../lib/index'
+
 import localStorage from '../lib/nodeLocalStorage'
-import { expect } from 'chai'
+import { Nodriza } from '../lib/index'
+// import { expect } from 'chai'
+// import _ from 'lodash'
 
+// Set local storage trasporter for node client
+global.localStorage = localStorage
 const params = {
-  hostname: 'bp.nodriza.io',
-  localStorage
+  hostname: 'bp.nodriza.io'
 }
-
 const nodriza = new Nodriza(params)
 
+for (let i in nodriza) {
+  let obj = nodriza[i]
+  console.log('\nnodriza.' + i, '-->', typeof obj)
+  // if (_.isObject(obj)) { 
+  //  for (let j in obj) {
+  //    console.log(' nodriza.' + i + '.' + j, '-->',typeof obj[j])
+  //  }
+  // }
+}
+console.log('->>> nodriza.api.user:', nodriza.api.user.get)
 nodriza.api.user.login({username: 'jprieto@nodriza.io', password: 'xxx'}, (err, user) => {
-	if (err) return console.log('->>> err:', err)
-	console.log('->>> user:', user)
-	nodriza.session.set(user)
-	console.log('->>> obj:', nodriza.session.get(user))
+  if (err) return console.log('->>> err:', err)
+  console.log('->>> user:', user)
+  nodriza.session.set(user)
+  console.log('->>> obj:', nodriza.session.get(user))
 })
 // describe('When user try to login with wrong password', function(){
 //   it('should return an array', function() {
-// 	nodriza.api.user.login({username: 'jprieto@nodriza.io', password: 'xxx'}, (err, user) => {
-// 		expect(err, 'User can login with wrong password').to.exist()
-// 	})
+//  nodriza.api.user.login({username: 'jprieto@nodriza.io', password: 'xxx'}, (err, user) => {
+//    expect(err, 'User can login with wrong password').to.exist()
+//  })
 //   })
 // })
