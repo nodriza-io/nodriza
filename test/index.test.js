77898b4df7364cd7ac32f3cdf434b57374b2474d
System Administrator <root@lapshox-2.local>
2017-08-10 04:30:16 -0500

    accessToken support added

--- test/index.test.js
+++ test/index.test.js
@@ -1,33 +1,19 @@
 'use strict'
 
-import localStorage from '../lib/nodeLocalStorage'
 import { Nodriza } from '../lib/index'
 // import { expect } from 'chai'
 // import _ from 'lodash'
-
-// Set local storage trasporter for node client
-global.localStorage = localStorage
 const params = {
-  hostname: 'bp.nodriza.io'
+  hostname: 'bp.nodriza.io',
+  accessToken: '-56a69869-bf0a-4650-98e9-fcd9680b31d5'
 }
 const nodriza = new Nodriza(params)
 
-for (let i in nodriza) {
-  let obj = nodriza[i]
-  console.log('\nnodriza.' + i, '-->', typeof obj)
-  // if (_.isObject(obj)) { 
-  //  for (let j in obj) {
-  //    console.log(' nodriza.' + i + '.' + j, '-->',typeof obj[j])
-  //  }
-  // }
-}
-console.log('->>> nodriza.api.user:', nodriza.api.user.get)
-nodriza.api.user.login({username: 'jprieto@nodriza.io', password: 'Shox009_'}, (err, user) => {
+nodriza.api.user.me((err, profile) => {
   if (err) return console.log('->>> err:', err)
-  console.log('->>> user:', user)
-  nodriza.session.set(user)
-  console.log('->>> obj:', nodriza.session.get(user))
+  console.log('->>> profile:', profile)
 })
+
 // describe('When user try to login with wrong password', function(){
 //   it('should return an array', function() {
 //  nodriza.api.user.login({username: 'jprieto@nodriza.io', password: 'xxx'}, (err, user) => {
@@ -35,3 +21,4 @@ nodriza.api.user.login({username: 'jprieto@nodriza.io', password: 'Shox009_'}, (
 //  })
 //   })
 // })
+
