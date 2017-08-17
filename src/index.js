a9fccdc606d009108f64e105d136e52e82652da5
Juan Carlos Prieto from Dev <jprieto@nodriza.io>
2017-08-17 11:46:20 -0500

    module exportes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 'use strict'
 
 import { Api } from './api'
-// import { Socket } from './socket'
 import { Session } from './session'
 import { Storage } from './storage'
 import { Utils } from './utils'
 
-export class Nodriza extends Api {
+class Nodriza extends Api {
   constructor (params) {
     super(params)
     if (!params || !params.hostname) throw new Error('Please define hostname -> e.g acme.nodriza.io')
@@ -14,6 +13,7 @@ export class Nodriza extends Api {
     this.session = new Session()
     this.storage = new Storage()
     this.u = new Utils()
-    // this.socket = new Socket(this)
   }
 }
+
+module.exports = Nodriza
