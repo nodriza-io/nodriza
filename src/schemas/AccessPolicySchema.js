fe0be678614222855df63ca855541ccb41642ca7
Juan Carlos Prieto from Dev <jprieto@nodriza.io>
2017-09-14 15:19:53 -0500

    schemas added

--- src/schemas/AccessPolicySchema.js
+++ src/schemas/AccessPolicySchema.js
@@ -4,14 +4,16 @@ module.exports = {
     "example": "594d4b5970abb08100bf5b71",
     "model": "app",
     "required": "true",
-    "mustExist": true
+    "mustExist": true,
+    "skipUpdate": true
   },
   "company": {
     "description": "Every user that match the policy will be asociated with this company and heritage the company role",
     "example": "59418bcd105605cc2693a981",
     "model": "company",
     "mustExist": true,
-    "required": true
+    "required": true,
+    "skipUpdate": true
   },
   "policy": {
     "description": "Access policy to match -> full domain 'nodriza.io' or 'jprieto@nodriza.io' for a single user",
@@ -19,7 +21,8 @@ module.exports = {
     "type": "string",
     "required": true,
     "existCheck": true,
-    "unique": true
+    "unique": true,
+    "skipUpdate": true
   },
   "ip": {
     "description": "Limit access to IP",
