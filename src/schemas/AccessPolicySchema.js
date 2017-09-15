a35ef2040542dcdc9f09e3472835764e0b9309fe
Juan Carlos Prieto from Dev <jprieto@nodriza.io>
2017-09-14 19:34:05 -0500

    updates

--- src/schemas/AccessPolicySchema.js
+++ src/schemas/AccessPolicySchema.js
@@ -4,16 +4,14 @@ module.exports = {
     "example": "594d4b5970abb08100bf5b71",
     "model": "app",
     "required": "true",
-    "mustExist": true,
-    "skipUpdate": true
+    "mustExist": true
   },
   "company": {
     "description": "Every user that match the policy will be asociated with this company and heritage the company role",
     "example": "59418bcd105605cc2693a981",
     "model": "company",
     "mustExist": true,
-    "required": true,
-    "skipUpdate": true
+    "required": true
   },
   "policy": {
     "description": "Access policy to match -> full domain 'nodriza.io' or 'jprieto@nodriza.io' for a single user",
@@ -21,17 +19,19 @@ module.exports = {
     "type": "string",
     "required": true,
     "existCheck": true,
-    "unique": true,
-    "skipUpdate": true
+    "unique": true
   },
   "ip": {
-    "description": "Limit access to IP",
-    "example": "192.168.0.1",
+    "description": "Restrict Site Access by IP or IP Range",
+    "example": [
+      "192.168.0.10-192.168.0.100",
+      "192.168.0.101"
+    ],
     "type": "string",
     "ip": true
   },
   "accessTime": {
-    "description": "Set access time rage",
+    "description": "Allow user access only in specific hours of the day",
     "example": {
       "from": 9,
       "to": 19
