46a6f869bcec394033a907071e009729868adb94
Juan Carlos Prieto from Dev <jprieto@nodriza.io>
2017-09-20 11:05:26 -0500

    query string fix

--- src/utils.js
+++ src/utils.js
@@ -54,6 +54,7 @@ export class Utils {
   }
 
   unCammel (str) {
+    if (typeof str !== 'string') return ''
     return str.replace(/([a-z])([A-Z])/g, '$1 $2').replace(/\b([A-Z]+)([A-Z])([a-z])/, '$1 $2$3').replace(/^./, function(str) {
       return str.toUpperCase()
     })
