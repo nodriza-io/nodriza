bffcdb645123c53007999921f5226310d32e00ce
Andrès Ariza <aariza@nodriza.io>
2017-10-26 17:11:49 -0500

    merge

--- src/schemas/QuoteSchema.js
+++ src/schemas/QuoteSchema.js
@@ -17,12 +17,11 @@ module.exports = {
     "type": "datetime",
     "skipAll": true
   },
-  "subject": {
+  "title": {
+    "description": "Title about quote",
     "required": true,
-    "displayName": true,
     "type": "string"
   },
-<<<<<<< HEAD
   "description": {
     "description": "Description about quote",
     "type": "string"
@@ -36,36 +35,5 @@ module.exports = {
     "description": "Destinatary by quote",
     "model": "user",
     "required": true
-=======
-  "proposal": {
-    "description": "Detailed quote proposal.",
-    "type": "string",
-    "html": true
-  },
-  "status": {
-    "required": true,
-    "type": "string",
-    "enum": [
-      "Draft",
-      "In Review",
-      "Presented",
-      "Acepted",
-      "Denied"
-    ]
-  },
-  "expirationDate": {
-    "description": "Expiration date determines when the quote expires",
-    "type": "datetime"
-  },
-  "relatedLead": {
-    "description": "Select the lead that is related with this quote",
-    "model": "lead",
-    "mustExist": true
-  },
-  "relatedUser": {
-    "description": "Select the user that is related with this quote",
-    "model": "user",
-    "mustExist": true
->>>>>>> 7629eb0e806d030ac5c341d1eeb6957823e05648
   }
 }
