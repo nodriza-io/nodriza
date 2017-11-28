4f61269aa7237ca622a5bae6d1816b866a720833
Jose Torrens <jtorrens@nodriza.io>
2017-11-28 23:18:14 +0000

    iops month provisioned

--- src/schemas/AwsIopsMonthProvisionedSchema.js
+++ src/schemas/AwsIopsMonthProvisionedSchema.js
@@ -1,19 +1,23 @@
 module.exports = {
+  "createdAt": {
+    "type": "datetime",
+    "skipAll": true
+  },
+  "updatedAt": {
+    "type": "datetime",
+    "skipAll": true
+  },
   "awsVolume": {},
   "iops": {
-    "type": "integer"
+    "type": "integer",
+    "skipAll": true
   },
   "regionName": {
     "skipAll": true,
     "model": "awsRegion"
   },
   "product": {
+    "skipAll": true,
     "model": "product"
-  },
-  "createdAt": {
-    "type": "datetime"
-  },
-  "updatedAt": {
-    "type": "datetime"
   }
 }
