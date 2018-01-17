44c1ac536667088a892436fbccadf3d895d6c814
Juan Carlos Prieto <jtorrens@nodriza.io>
2018-01-17 15:26:50 +0000

    convert quote to invoice

--- src/api/QuoteApi.js
+++ src/api/QuoteApi.js
@@ -29,4 +29,21 @@ export class Quote extends Request {
       callback(null, data)
     })
   }
+  /**
+  * POST /v1/quote/generateInvoice [PRIVATE]
+  *
+  * Generate Invoice from quote
+  *
+  * @param  {Object} params
+  * - @param  {String} quoteId * -> (In Body)  e.g 5a5e5c5c321c3f05a1b7fce5
+  * @param  {Function} callback
+  * @return {Code} 200, 400, 403
+  */
+  generateInvoice (body, callback) {
+    let params = {url: '/v1/quote/generateInvoice', body}
+    this.post(params, (err, data) => {
+      if (err) return callback(err)
+      callback(null, data)
+    })
+  }
 }
