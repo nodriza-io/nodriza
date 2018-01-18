046f2024c1226ba0d78f705df19462fc3b4ea4b3
Juan Carlos Prieto <jtorrens@nodriza.io>
2018-01-18 20:49:38 +0000

    article

--- src/schemas/ArticleSchema.js
+++ src/schemas/ArticleSchema.js
@@ -18,5 +18,34 @@ module.exports = {
   "updatedAt": {
     "type": "datetime",
     "skipAll": true
+  },
+  "title": {
+    "required": true,
+    "type": "string"
+  },
+  "category": {
+    "model": "category",
+    "mustExist": true,
+    "required": true
+  },
+  "layout": {
+    "description": "Article layout",
+    "required": true,
+    "type": "string",
+    "html": true,
+    "target": "template",
+    "query": {
+      "type": "article"
+    }
+  },
+  "content": {
+    "description": "Article content.",
+    "required": true,
+    "type": "string",
+    "html": true,
+    "target": "template",
+    "query": {
+      "type": "article"
+    }
   }
 }
