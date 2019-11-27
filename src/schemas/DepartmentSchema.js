83f8d3c680fe32ff52902f0f36fb36e63c1914ad
Juan Carlos Prieto <jc@nodriza.io>
2019-11-27 19:53:33 +0000

    modified deparment schema

--- src/schemas/DepartmentSchema.js
+++ src/schemas/DepartmentSchema.js
@@ -7,14 +7,24 @@ module.exports = {
     "type": "datetime",
     "skipAll": true
   },
-  "name": {
-    "description": "Department name",
+  "keyName": {
+    "description": "Unique key name identifier for URL. Only alphanumeric, dash and underscore allowed",
     "example": "Management",
     "required": true,
     "type": "string",
     "unique": true,
+    "primaryKey": true,
+    "regex": "/^([a-zA-Z0-9_-]+)$/",
     "displayName": true,
     "existCheck": true
+  },
+  "name": {
+    "description": "Department name",
+    "example": "Pets",
+    "required": true,
+    "type": "string",
+    "unique": true,
+    "displayName": true
   },
   "description": {
     "description": "Department description",
