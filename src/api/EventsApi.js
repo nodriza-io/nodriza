97d241a01b931f14a25b84acab8a7c8cfd3eb4fd
Juan Carlos Prieto <jc@nodriza.io>
2022-10-04 17:54:00 +0000

    update events queries

--- src/api/EventsApi.js
+++ src/api/EventsApi.js
@@ -14,16 +14,18 @@ export class Events extends Request {
   /**
   * GET /v1/events/docEvents [PRIVATE]
   *
-  * Get events by doc id
+  * Return an array with events by doc id
   *
   * @param  {Object} params
   * - @param  {String} id * -> (In query) Register id
-  * - @param  {String}  * -> (In query) Doc id
+  * - @param  {String} docId * -> (In query) Doc id
   * @param  {Function} callback
   * @return {Code} 200, 400, 403
   */
-  docEvents (callback) {
-    this.get('/v1/events/docEvents', (err, data) => {
+  docEvents (params, callback) {
+    let url = '/v1/events/docEvents'
+    if (!_.isEmpty(params)) url += '?' + queryString.stringify(params)
+    this.get(url, (err, data) => {
       if (err) return callback(err)
       callback(null, data)
     })
